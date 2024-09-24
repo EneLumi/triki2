adcc0cfcba1476aea61194d78cab4413f5abbdea
EneLumi <ene.lumi@mail.ee>
2024-09-24 16:05:50 +0300

    Create urls and utils in usermanager

---
+++ usermanager/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+
+# ühendab view-sid ja html-e
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register_user, name="register"),
+    path("", include("game.urls")), # ütleb djangole, et ta vaataks ka game app-is olevad urls faile
+]
